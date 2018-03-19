1ff60259fdb4bc09e14bb7ff55f9aeff93e37efa
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2018-03-19 20:39:49 +0700

    change bootstrap to grid-only

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap/scss/bootstrap-grid.scss'
 
 Vue.config.productionTip = false
 
