23cb9a4830e68e9cbf8df4f7bf85535e1eeab651
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2018-03-19 21:58:12 +0700

    add vue carousel

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,22 +4,35 @@
       <h3 class="text-center">
         {{msg}}
       </h3>
+      <div class="row">
+        <carousel :perPage=1>
+          <slide>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque rem suscipit eum iure. Facere numquam, fugiat aspernatur quibusdam
+            ea autem magnam quis architecto rem impedit fuga enim vel sed maxime?
+          </slide>
+          <slide>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus illo aliquam vero assumenda unde aut laudantium non! Dolor,
+            voluptatum ipsam, deleniti magni, quaerat quasi placeat nemo pariatur ab aspernatur tempore.
+          </slide>
+        </carousel>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import {Carousel, Slide} from 'vue-carousel'
 export default {
   name: 'Home',
   data () {
     return {
       msg: 'Home Page'
     }
-  }
+  },
+  components: {Carousel, Slide}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
