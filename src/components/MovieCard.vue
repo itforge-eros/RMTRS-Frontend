3f582aff20a12c3e273cca96ae21f905cdd1bd64
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2018-03-24 00:47:04 +0700

    patch MovieCard design

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="movie-card col-4">
+    <div class="movie-card col-12 col-sm-6 col-md-4 col-xl-3">
         <div class="img-wall">
             <img :src="img">
         </div>
@@ -21,13 +21,20 @@ export default {
     .movie-card {
         width: 320px;
         height: 400px;
+        border-radius: $main-round;
+        overflow: hidden;
         .img-wall {
             width: 100%;
             height: 73%;
             background: linear-gradient(125deg, rgb(2, 109, 255), rgb(107, 204, 255));
             overflow: hidden;
+            border-radius: $main-round;
             img {
-                width: 110%;
+                display: block;
+                min-width: 100%;
+                min-height: 100%;
+                width: 100%;
+                height: auto;
                 position: relative;
             }
         }
