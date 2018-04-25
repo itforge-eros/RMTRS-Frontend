fa6ebccdd608b7f327e592bfc1c2cdc7547e31ad
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2018-04-25 20:41:39 +0700

    Change API_URL

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const API_URL = window.location.hostname === 'rmtrs.itforge.io'
-  ? 'http://rmtrs.itforge.io:8888' : 'http://localhost:8888'
+const API_URL = 'http://rmtrs.itforge.io:8888'
 
 export default axios.create({
   baseURL: API_URL,
