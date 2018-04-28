486c39c673d5f8b86b918fe1d1a76e450a2c2b1f
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2018-04-28 21:02:18 +0700

    add table to account page

--- src/pages/console/section/Account.vue
+++ src/pages/console/section/Account.vue
@@ -1,9 +1,60 @@
 <template>
-  <p>Placeholder</p>
+  <div class="row">
+    <div class="col-12">
+      <vuetable ref="vuetable"
+        api-url="http://rmtrs.itforge.io:8888/account/paged?page=0"
+        :fields="columns"
+        :css="tableMeta"
+        pagination-path="">
+
+        <template slot="name" slot-scope="props">
+          <p>{{ props.rowData.title }} {{ props.rowData.fname }} {{ props.rowData.lname }}</p>
+        </template>
+
+        <template slot="action" slot-scope="props">
+          <router-link tag="button" style="color: blue" class="btn" :to="'account/edit/'+props.rowData.id">Edit</router-link>
+          <router-link tag="button" style="color: red" class="btn" :to="'account/delete/'+props.rowData.id">Delete</router-link>
+        </template>
+
+      </vuetable>
+    </div>
+    <div class="add-btn">
+      <span>+</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
 export default {
-  name: 'Account'
+  name: 'Account',
+  components: {Vuetable},
+  data () {
+    return {
+      tableMeta: {
+        tableClass: 'table table-striped'
+      },
+      columns: [
+        {
+          name: '__slot:name',
+          title: 'Full Name',
+          titleClass: 'text-center',
+          dataClass: 'center-row'
+        },
+        {
+          name: 'role',
+          title: 'Role',
+          titleClass: 'text-center',
+          dataClass: 'text-center center-row'
+        },
+        {
+          name: '__slot:action',
+          title: 'Action',
+          titleClass: 'text-center',
+          dataClass: 'text-center center-row'
+        }
+      ]
+    }
+  }
 }
 </script>
