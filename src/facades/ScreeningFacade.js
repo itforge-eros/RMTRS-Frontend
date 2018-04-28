940cbadf8cac3f13c5a14229829c31fa7a4daec6
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2018-04-28 12:48:55 +0700

    Add method for get movie by id

--- src/facades/ScreeningFacade.js
+++ src/facades/ScreeningFacade.js
@@ -1,6 +1,10 @@
 import axios from '../config/axios.config'
 
 export default new class ScreeningFacade {
+  getMovieAvailable = id => {
+    return axios.get(`/movie/available/${id}`)
+  }
+
   getMovie = id => {
     return axios.get(`/movie/${id}`)
   }
